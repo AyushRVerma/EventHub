5f3516efc9a8b355f8e36d11e48eecbb489ea6a8
AyushRVerma <ayushrajverma67@gmail.com>
2026-07-21 11:49:49 +0530

    More exceptions added  & implement @Future

--- src/main/java/com/eventbook/EventHub/repositories/EventRepository.java
+++ src/main/java/com/eventbook/EventHub/repositories/EventRepository.java
@@ -16,7 +16,7 @@ import java.util.UUID;
 public interface EventRepository extends JpaRepository<Event, UUID> {
 
     Page<Event> findByOrganizerId(UUID organizerId, Pageable pageable);
-    Optional<Event>     findByIdAndOrganizerId(UUID id, UUID organizerId);
+    Optional<Event> findByIdAndOrganizerId(UUID id, UUID organizerId);
     Page<Event> findByStatus(EventStatusEnum status, Pageable pageable);
 
 //    @Query(value= "SELECT * FROM events WHERE " +
